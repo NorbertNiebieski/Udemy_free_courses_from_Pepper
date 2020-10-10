274d8646a1cc9cfcf8f0120ae2e4feea9b1cbd6f
Norbert Nawrocki <norek784@gmail.com>
2020-10-10 23:01:19 +0200

    adding writeing url

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import private_date
 
 if __name__ == '__main__':
 
-    my_bot = pepper_free_course.PepperBot()
-
     # in the another python file you could do your on file with this string or write your login and password below
     udemy_login = private_date.udemy_login
     udemy_password = private_date.udemy_password
 
     # Write pepper promotion url below
-    url = ""
+    #url = ""
+
+    url = input("Pepper's promotion url: ")
 
     # depends of your internet connection
-    sleep_time = 10
+    sleep_time = 5
+
+    my_bot = pepper_free_course.PepperBot()
 
     # taking links from pepper
     links = my_bot.talking_links_to_udemy_from_pepper_promotion(url)
